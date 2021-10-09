2f61068c3d10eb1bbf153d09a13f229adfa6c605
Dan Tran <dantn93@gmail.com>
2021-10-10 00:32:54 +0700

    init

--- src/views/home.js
+++ src/views/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -7,7 +7,38 @@ import {
   Image
 } from 'react-native';
 
+
+const CallAPI = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({data: "Data"})
+      console.log("Đã trả về data")
+    }, 3000);
+  }) 
+}
+
+const getData = async (setData) => {
+  let data = await CallAPI()
+  setData(data);
+}
+
 export default Home = ({ route, navigation }) => {
+  const [value, setValue] = useState(1);
+  const [data, setData] = useState({data: null})
+  
+  useEffect(() => {
+    console.log("Mới vào màn hình")
+    getData(setData)
+
+    return () => {
+      console.log("Huỷ màn hình này")
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log("Data đã lắng nghe là: ", data);
+  }, [data])
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {/* Header */}
@@ -24,6 +55,17 @@ export default Home = ({ route, navigation }) => {
       </View>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 30 }}>Home screen</Text>
+
+        <Text style={{fontSize: 60, marginTop: 100}}>{value}</Text>
+        <TouchableOpacity
+          style={{width: '50%', height: 50, justifyContent: 'center', alignItems: 'center', borderRadius: 100, marginTop: 50, backgroundColor: "#321A9A"}}
+          onPress={() => {
+            let newValue =  value + 1;
+            setValue(newValue);
+          }}  
+        >
+          <Text style={{color: 'white', fontSize: 20}}>Đếm số</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
