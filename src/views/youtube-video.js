93f721c4933cfbd5999c897000d49f1f9b585a1b
dan <dantn93@gmail.com>
2022-04-05 22:45:26 +0700

    firebaseSDK

--- src/views/youtube-video.js
+++ src/views/youtube-video.js
@@ -15,7 +15,7 @@ export default YoutubePlayer = () => {
 		<SafeAreaView style={{ flex: 1 }}>
 			<View style={{ height: '100%', width: '100%' }}>
 				<Text>Video Player</Text>
-				<YouTube
+				{/* <YouTube
 					apiKey='AIzaSyD6FWgL-E8Cglt_J0DH3-TDSfXhNpaZiy0'
 					videoId="KVZ-P-ZI6W4" // The YouTube video ID
 					play={false} // control playback of video with true/false
@@ -26,7 +26,7 @@ export default YoutubePlayer = () => {
 					onChangeQuality={e => setQuality(e.quality)}
 					onError={e => setError(e.error)}
 					style={{ alignSelf: 'stretch', height: 300 }}
-				/>
+				/> */}
 			</View>
 		</SafeAreaView>
 	)
